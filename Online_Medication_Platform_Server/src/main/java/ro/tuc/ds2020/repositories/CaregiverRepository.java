b490cb790c2324f2c8973bacebf33fd23aeb0945
Catalin Sendrea <88668067+catalinsendrea@users.noreply.github.com>
2021-08-09 17:15:56 +0300

    Delete Online_Medication_Platform_Server directory

--- Online_Medication_Platform_Server/src/main/java/ro/tuc/ds2020/repositories/CaregiverRepository.java
+++
@@ -1,9 +0,0 @@
-package ro.tuc.ds2020.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ro.tuc.ds2020.entities.Caregiver;
-
-@Repository
-public interface CaregiverRepository extends JpaRepository<Caregiver, Integer> {
-}
--- Online_Medication_Platform_Server/src/main/java/ro/tuc/ds2020/repositories/ListOfMedicationRepository.java
+++
@@ -1,9 +0,0 @@
-package ro.tuc.ds2020.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ro.tuc.ds2020.entities.ListOfMedication;
-
-@Repository
-public interface ListOfMedicationRepository extends JpaRepository<ListOfMedication, Integer> {
-}
